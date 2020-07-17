4f3a207eeb4582885ddbd6d9c0224594a333884e
sudheera1223 <kondareddy.yaramala@gmail.com>
2020-07-17 13:59:41 -0500

    Added a sample example

--- src/app/one.directive.ts
+++ src/app/one.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appOne]'
+  selector: '[dir]'
 })
 export class OneDirective {
 
-  constructor() { }
+  constructor(private elmRef: ElementRef, private renderer2: Renderer2) { 
+    console.log('Directive Initialized')
+  }
+
+  ngAfterViewInit(){
+    this.renderer2.setProperty(this.elmRef.nativeElement, 'value', 'New Value');
+  }
 
 }
