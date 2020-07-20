4b1b0194360131f512ad6b947159a699590cd10d
sudheera1223 <kondareddy.yaramala@gmail.com>
2020-07-20 14:32:16 -0500

    Added multiple directive instances on an element

--- src/app/square.directive.ts
+++ src/app/square.directive.ts
@@ -1,18 +1,19 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[appSquare]'
 })
 export class SquareDirective {
+  @Input('appSquare') multiply: number;
 
   constructor(private elmRef: ElementRef<HTMLInputElement>,
     private renderer2: Renderer2) { }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     const value = Number(this.elmRef.nativeElement.value);
-    if(!Number.isNaN(value)){
+    console.log(this.multiply);
+    if (!Number.isNaN(value)) {
       this.renderer2.setProperty(this.elmRef.nativeElement, 'value', value * value);
     }
   }
-
 }
